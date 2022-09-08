d14ec5dcb44f9bafbdaa471e975d1539c6654b8f
Adam Balicki <me@mekansm.io>
2022-09-08 00:16:47 -0500

    Helm chart to deploy a cluster

--- src/ClusterExample/Program.cs
+++ src/ClusterExample/Program.cs
@@ -4,10 +4,16 @@ using DotNext.Net.Cluster.Consensus.Raft;
 using DotNext.Net.Cluster.Consensus.Raft.Http;
 using Microsoft.AspNetCore.Connections;
 using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+
+await ServiceDiscoveryReady();
 
 var scheme = "http";
 var hostname = GetHostName();
 var port = GetPort();
+
+Console.WriteLine($"host: {scheme}://{hostname}:{port}");
 
 var configuration = new Dictionary<string, string>
             {
@@ -27,6 +33,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddInMemoryCollection(configuration);
 
 builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
 builder.Services.AddSwaggerGen();
 
 builder.Services.UseInMemoryConfigurationStorage(AddClusterMembers);
@@ -41,6 +48,7 @@ app.UseSwaggerUI();
 app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
+    endpoints.MapHealthChecks("/healthz");
     endpoints.MapSwagger();
     endpoints.MapControllers();
 });
@@ -90,7 +98,51 @@ void AddClusterMembers(IDictionary<ClusterMemberId, UriEndPoint> members)
     }
     else
     {
-        // dns service discovery
+        var test = Dns.GetHostEntry("serverfault.com", AddressFamily.InterNetwork);
+
+        Console.WriteLine($"test: {test.AddressList.Length}");
+
+        var entry = Dns.GetHostEntry("cluster-example-discovery", AddressFamily.InterNetwork);
+
+        Console.WriteLine($"disc: {entry.AddressList.Length}");
+
+        foreach (var ip in entry.AddressList)
+        {
+            var url = new Uri($"{scheme}://{ip}:{port}", UriKind.Absolute);
+            var address = new UriEndPoint(url);
+
+            Console.WriteLine($"disc: {url}");
+
+            members.Add(ClusterMemberId.FromEndPoint(address), address);
+        }
+    }
+}
+
+async Task ServiceDiscoveryReady()
+{
+    var nodes = Environment.GetEnvironmentVariable("CLUSTER_NODE_COUNT");
+
+    if (!string.IsNullOrWhiteSpace(nodes))
+    {
+        var nodeCount = int.Parse(nodes);
+
+        Console.WriteLine($"Waiting for {nodeCount} nodes to start...");
+
+        for(var i = 0; i< 60; i++)
+        {
+            try
+            {
+                var entry = Dns.GetHostEntry("cluster-example-discovery", AddressFamily.InterNetwork);
+
+                if (entry.AddressList.Length == nodeCount) return;
+            }
+            catch
+            {
+
+            }
+
+            await Task.Delay(1000);
+        }
     }
 }
 
